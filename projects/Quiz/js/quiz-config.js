b85a60c80c9e075cd4dfead683f8e7f9b442f83a
Idakuhlman <73815943+Idakuhlman@users.noreply.github.com>
2022-01-09 16:58:20 +0200

    Add files via upload

---
+++ projects/Quiz/js/quiz-config.js
@@ -0,0 +1,79 @@
+questions = [
+    {
+        question: 'How old I am?',
+        answers: {
+            a: '20',
+            b: '26',
+            c: '23'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: 'In which programming language I am the best',
+        answers: {
+            a: 'JavaScript',
+            b: 'Python',
+            c: 'PHP'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: 'What I like to do at freetime',
+        answers: {
+            a: 'Bake',
+            b: 'sleep',
+            c: 'paint'
+        },
+        correctAnswer: 'a'
+    },
+    {
+    question: 'How many pets I have',
+    answers: {
+        a: '4',
+        b: '3',
+        c: '1'
+    },
+    correctAnswer: 'b'
+},
+{
+    question: 'Where I want to live in the future',
+    answers: {
+        a: 'New York',
+        b: 'City',
+        c: 'Coyntryside'
+    },
+    correctAnswer: 'c'
+},
+{
+    question: 'What is my other occupation but now as a hobby',
+    answers: {
+        a: 'Pastry chef',
+        b: 'Builder',
+        c: 'Real estate agent'
+    },
+    correctAnswer: 'a'
+}
+
+
+
+
+];
+
+verdicts = [
+    {
+        minScore: 6,
+        text: 'You knew everything!'
+    },
+    {
+    minScore: 3,
+    text: 'That is nice!'
+    },
+    {
+        minScore: 1,
+        text: 'You have to study a little bit more about me'
+    },
+    {
+        minScore: 0,
+        text: 'Noo bad luck.'
+    }
+];
